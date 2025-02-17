018eabb2d911baa74cd248a867b41ec96a0d77f9
magnus.coder <kelimbth@gmail.com>
2025-02-17 12:14:44 +0300

    updated main() to initialize Firebase when app is launched

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_phone_auth/firebase_options.dart';
 import 'package:flutter_phone_auth/screens/onboarding_screen.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
